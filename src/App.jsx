d88d1c5d2827a96ed8dfc439607ab2c7060ab23e
Calvin Shio <shiocorp49@gmail.com>
2024-03-29 20:12:36 +0300

    sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-export default function App() {
+function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+        // relative position to it's container
+        <main className={"relative"}>
+            nav
+            <section className={"xl:padding-1 wide:padding-r padding-b"}>
+                Hero
+            </section>
+            <section className={"padding"}>
+                Popular products
+            </section>
+            <section className={"padding"}>
+                super quality
+            </section>
+            <section className={"padding-x py-10"}>
+                services
+            </section>
+            <section className={"padding"}>
+                special order
+            </section>
+            <section className={"padding bg-pale-blue"}>
+                customers review
+            </section>
+            <section className={"padding padding-x sm:py-32 py-16 w-full"}>
+                subscribe
+            </section>
+            <section className={"padding bg-black padding-x padding-t pb-8"}>
+                footer
+            </section>
+        </main>
+    );
 }
+
+export default App;
