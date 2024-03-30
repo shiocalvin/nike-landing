68871d67ffafeb0f68919bf1ea393dfcbcfb82fc
Calvin Shio <shiocorp49@gmail.com>
2024-03-30 21:22:39 +0300

    services tags

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,13 @@
+import {services} from "../assets/Constants/index.js";
+import ServiceCard from "../Components/ServiceCard.jsx";
+
 function Services() {
     return (
-        <div>Services</div>
+        <section className={'max-container flex justify-center flex-wrap gap-9'}>
+            {services.map((service) => (
+                <ServiceCard key={service?.label} {...service}/>
+            ))}
+        </section>
     );
 }
 
