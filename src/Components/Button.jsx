3cb06f6ebc45c99c7157b9256bbc7504fceb2dac
Calvin Shio <shiocorp49@gmail.com>
2024-03-30 23:07:32 +0300

    subsribe

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,6 @@
-function Button({label, iconUrl, backGroundColor, borderColor, textColor}) {
+function Button({label, iconUrl, fullWidth, backGroundColor, borderColor, textColor}) {
     return (
-        <button className={`flex justify-center items-center px-7 py-4 border font-montserrat leading-none  rounded-full ${backGroundColor ? `${backGroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'}`}>
+        <button className={`flex justify-center items-center px-7 py-4 border font-montserrat leading-none  rounded-full ${backGroundColor ? `${backGroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red '} ${fullWidth && 'w-full'}`}>
             {label}
             {iconUrl && <img src={iconUrl} alt="/" className={'ml-2 rounded-full w-5 h-5'}/>}
         </button>
