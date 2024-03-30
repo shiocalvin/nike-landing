eefa93d9c0f729444132898e6bd2ddf5b358cd02
Calvin Shio <shiocorp49@gmail.com>
2024-03-30 22:39:02 +0300

    customer reviews

---
+++ src/Components/ReviewCard.jsx
@@ -0,0 +1,17 @@
+import {star} from "../assets/icons/index.js";
+
+function ReviewCard({imgURL, customerName, rating, feedback}) {
+    return (
+        <div className={'flex justify-center items-center flex-col'}>
+            <img src={imgURL} alt="customer-img" className={'rounded-full object-cover w-[120px] h-[120px]'}/>
+            <p className={'mt-6 max-w-sm text-center info-text'}>{feedback}</p>
+            <div className={'flex mt-3 justify-center items-center gap-2.5'}>
+                <img src={star} alt="rating-start" width={24} height={24} className={'m-0 object-contain'}/>
+                <p className={'text-xl font-montserrat text-slate-gray'}>{rating}</p>
+            </div>
+            <h3 className={'mt-1 font-palanquin text-3xl text-center font-bold'}>{customerName}</h3>
+        </div>
+    );
+}
+
+export default ReviewCard;
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,23 @@
+import {reviews} from "../assets/Constants/index.js";
+import ReviewCard from "../Components/ReviewCard.jsx";
+
 function CustomerReviews() {
     return (
-        <div>Customer Reviews</div>
+        <section className={'max-container'}>
+            <h3 className={'font-palanquin text-center text-4xl font-bold'}>
+                What Our
+                <span className={'text-coral-red'}> Customers </span>
+                Say?
+            </h3>
+            <p className={'info-text m-auto  mt-4 max-w-lg text-center'}>
+                Hear genuine stories from our satisfied customers about their exceptional experiences with us.
+            </p>
+            <div className={'mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'}>
+                {reviews.map((review) => (
+                    <ReviewCard {...review} key={review.customerName}/>
+                ))}
+            </div>
+        </section>
     );
 }
 
