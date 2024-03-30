449e365398adf9776cb9b74131f66eddae74c83b
Calvin Shio <shiocorp49@gmail.com>
2024-03-30 22:21:37 +0300

    special offers

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,6 @@
-function Button({label, iconUrl}) {
+function Button({label, iconUrl, backGroundColor, borderColor, textColor}) {
     return (
-        <button className={'flex justify-center items-center px-7 py-4 border font-montserrat leading-none bg-coral-red rounded-full text-white border-coral-red'}>
+        <button className={`flex justify-center items-center px-7 py-4 border font-montserrat leading-none  rounded-full ${backGroundColor ? `${backGroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'}`}>
             {label}
             {iconUrl && <img src={iconUrl} alt="/" className={'ml-2 rounded-full w-5 h-5'}/>}
         </button>
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,6 +1,35 @@
+import {offer} from "../assets/images/index.js";
+import Button from "../Components/Button.jsx";
+import {arrowRight} from "../assets/icons/index.js";
+
 function SpecialOffers() {
     return (
-        <div>Offers</div>
+        <section className={'flex flex-wrap items-center max-xl:flex-col-reverse gap-10 max-container'}>
+            <div className={'flex-1'}>
+                <img src={offer} width={773} height={687} className={'object-contain w-full'} alt="/"/>
+            </div>
+            <div className={'flex flex-1 flex-col'}>
+                <h2 className={'font-palanquin text-4xl capitalize font-bold lg:max-w-lg'}>
+                    <span className={'text-coral-red'}> Special </span> Offer
+                </h2>
+                {/*set max-with-sm for a tight block*/}
+                <p className={'mt-4 lg:max-w-lg info-text'}>Discover
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dignissimos dolor
+                    facere illum iure iusto molestias neque numquam placeat possimus quae, repellendus,
+                    reprehenderit sed sequi sunt temporibus veritatis voluptatibus!
+                </p>
+                {/*set max-with-sm for a tight block*/}
+                <p className={'mt-4 lg:max-w-lg info-text'}>Discover
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dignissimos dolor
+                    facere illum iure iusto molestias neque numquam placeat possimus quae, repellendus,
+                    reprehenderit sed sequi sunt temporibus veritatis voluptatibus!
+                </p>
+                <div className={'mt-11 flex flex-wrap gap-4'}>
+                    <Button label={"Shop Now"} iconUrl={arrowRight}/>
+                    <Button label={"Learn Now"} backGroundColor={'bg-white'} borderColor={'border-slate-gray'} textColor={'text-slate-gray'}/>
+                </div>
+            </div>
+        </section>
     );
 }
 
